c070d38b6e0392b97ded0937d6feecdd014ba140
Unai Aguilera <unai.aguilera@deusto.es>
2014-08-13 11:00:43 +0200

    Iteration reorganized

--- trabajo/sol_basica.py
+++ trabajo/sol_basica.py
@@ -20,7 +20,7 @@ dt = dx / c
 
 pasos = 25
 
-tabla = []
+tabla_y = []
 
 #condiciones iniciales para la y
 init_nodos = []
@@ -31,63 +31,63 @@ for i in range(n_nodos):
 	else:
 		init_nodos.append(1 / 2.0 * (1 - x))
 
-tabla.append(init_nodos)
+tabla_y.append(init_nodos)
 
-# primer paso
-nodos = []
-for i in range(n_nodos):
-	if i == 0 or i == (n_nodos - 1):
-		nodos.append(init_nodos[i])
-	else:
-		x = dx * i
-		nuevo_valor = (init_nodos[i + 1] + init_nodos[i - 1]) / 2.0 + dt * x * (x - 1)  
-		nodos.append(nuevo_valor)
-
-tabla.append(nodos)
-
-# pasos siguientes
-for p in range(1, pasos):
+#pasos temporales
+for p in range(pasos):
 	nodos = []
-	for i in range(n_nodos):
-		if i == 0 or i == (n_nodos - 1):
-			nodos.append(tabla[p][i])
-		else:
-			nuevo_valor = tabla[p][i + 1] + tabla[p][i - 1] - tabla[p - 1][i]
-			nodos.append(nuevo_valor)
-	tabla.append(nodos)
+	if p == 0:
+		# primer paso
+		for i in range(n_nodos):
+			if i == 0 or i == (n_nodos - 1):
+				nodos.append(init_nodos[i])
+			else:
+				x = dx * i
+				nuevo_valor = (init_nodos[i + 1] + init_nodos[i - 1]) / 2.0 + dt * x * (x - 1)  
+				nodos.append(nuevo_valor)
+	else:
+		#pasos siguientes
+		for i in range(n_nodos):
+			if i == 0 or i == (n_nodos - 1):
+				nodos.append(tabla_y[p][i])
+			else:
+				nuevo_valor = tabla_y[p][i + 1] + tabla_y[p][i - 1] - tabla_y[p - 1][i]
+				nodos.append(nuevo_valor)
+
+	tabla_y.append(nodos)
 
 print "dx = %.4f" % dx
 print "dt = %.4f" % dt
 print "f = %.2f" % (1 / (20 * 0.0071))
 
-# print "\\begin{tabular}{%s }" % (" c" * (len(tabla[0]) + 1))
+# print "\\begin{tabular}{%s }" % (" c" * (len(tabla_y[0]) + 1))
 # print "\hline"
 # print "Paso",
-# for i in range(len(tabla[0])):
+# for i in range(len(tabla_y[0])):
 # 	print "& %.2f" % (dx * i),
 # print "\\\\"	
 # print "\hline"
 # print "\hline"
-# for i in range(len(tabla)):
+# for i in range(len(tabla_y)):
 # 	print i,
-# 	for n in range(len(tabla[i])):
-# 		print "& %.2f" % tabla[i][n],
+# 	for n in range(len(tabla_y[i])):
+# 		print "& %.2f" % tabla_y[i][n],
 # 	print "\\\\"
 # print "\hline"
 # print "\\end{tabular}"
 
 # fig, ax = plt.subplots()
 
-# for s in range(len(tabla)):
+# for s in range(len(tabla_y)):
 # 	if s == 0:
 # 		x_values = [dx * i for i in range(n_nodos)]
-# 		y_values = tabla[s]
+# 		y_values = tabla_y[s]
 # 		points, = ax.plot(x_values, y_values, marker='o', linestyle='-')
 # 		ax.set_xlim(0, 1) 
 # 		ax.set_ylim(-2.0, 2.0) 
 # 	else:
 # 		new_x_values = [dx * i for i in range(n_nodos)]
-# 		new_y_values = tabla[s]
+# 		new_y_values = tabla_y[s]
 # 		points.set_data(new_x_values, new_y_values)
 
 # 	plt.pause(0.005)
@@ -101,11 +101,11 @@ for i in range(1, columns):
 print "\\\\"	
 print "\hline"
 print "\hline"
-for i in range(len(tabla)):
+for i in range(len(tabla_y)):
 	print i,
 	t = dt * i
 	_, sol_analitica = get_values(t, num_div, c)
 	for n in range(1, columns):
-		print "& %.4f (%.4f)" % (tabla[i][n], abs(tabla[i][n] - sol_analitica[n])),
+		print "& %.4f (%.4f)" % (tabla_y[i][n], abs(tabla_y[i][n] - sol_analitica[n])),
 	print "\\\\"
 print "\\end{tabular}"
